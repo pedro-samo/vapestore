f860601c85e169aed9c1af806e898839340f47b4
Pedro Morais <pedro.s.morais@gmail.com>
2020-11-02 22:00:13 -0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+function modalEntrar() {
+    let modalContainer = document.getElementById('modalContainerAge').classList.add('fechar');
+}
+
+function modalFechar() {
+    window.location.href = "http://www.google.com.br";
+}
